0167c1810415d582ddfd604bc93e2f878c5eb994
petercolella <peter.joseph.colella@gmail.com>
2018-08-05 23:32:20 -0400

    Lecture 244

--- redux-basics.js
+++ redux-basics.js
@@ -26,9 +26,12 @@ const rootReducer = ( state = initialState, action ) => {
 const store = createStore( rootReducer );
 console.log( store.getState() );
 
+// Subscription
+store.subscribe( () => {
+    console.log( '[Subscription]', store.getState() );
+} );
+
 // dispatching Action
 store.dispatch( { type: 'INC_COUNTER' } );
 store.dispatch( { type: 'ADD_COUNTER', value: 10 } );
 console.log( store.getState() );
-
-// Subscription
