846e57cbed89f16a71a4e75aab77dd7e47dcbc08
petercolella <peter.joseph.colella@gmail.com>
2018-08-08 00:20:41 -0400

    Lecture 251

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -7,7 +7,7 @@ const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case 'INCREMENT':
             const newState = Object.assign( {}, state );
-            newState.counter = state.conter + 1
+            newState.counter = state.counter + 1
             return newState;
         case 'DECREMENT':
             return {
@@ -29,7 +29,15 @@ const reducer = ( state = initialState, action ) => {
                 ...state,
                 results: state.results.concat( { id: new Date(), value: state.counter } )
             }
-
+        case 'DELETE_RESULT':
+            // const id = 2;
+            // const newArray = [...state.results];
+            // newArray.splice(id, 1)
+            const updatedArray = state.results.filter( result => result.id !== action.resultElId );
+            return {
+                ...state,
+                results: updatedArray
+            }
         default:
             return state
     }
