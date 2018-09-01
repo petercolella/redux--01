e836bcdf8b8872e5f482236374dade648e4a324d
petercolella <peter.joseph.colella@gmail.com>
2018-08-31 23:42:54 -0400

    npm audit fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
 import counterReducer from './store/reducers/counter';
@@ -12,9 +12,20 @@ import registerServiceWorker from './registerServiceWorker';
 const rootReducer = combineReducers( {
     ctr: counterReducer,
     res: resultReducer
-} )
+} );
 
-const store = createStore( rootReducer );
+const logger = store => {
+    return next => {
+        return action => {
+            console.log( '[Middleware] Dispatching', action );
+            const result = next( action );
+            console.log( '[Middleware] next state', store.getState() );
+            return result;
+        }
+    }
+};
 
-ReactDOM.render( <Provider store={store}><App /></Provider>, document.getElementById( 'root' ) );
+const store = createStore( rootReducer, applyMiddleware( logger ) );
+
+ReactDOM.render( <Provider store={store}> <App /></Provider>, document.getElementById( 'root' ) );
 registerServiceWorker();
