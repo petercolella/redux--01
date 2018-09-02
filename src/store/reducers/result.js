2a9f4ae3f1c463b60097a82086db995b58ad0fa4
petercolella <peter.joseph.colella@gmail.com>
2018-09-01 23:56:55 -0400

    Lecture 277

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -7,6 +7,7 @@ const initialState = {
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.STORE_RESULT:
+            // Change data
             return {
                 ...state,
                 results: state.results.concat( { id: new Date(), value: action.result } )
